af45b8977ed3a8748abd27399d4afca172c6e659
Victoria Adeniran <victoriaadeniran@Victorias-MacBook-Air.local>
2023-07-04 20:00:07 +0100

    While loops

--- session-5/index.js
+++ session-5/index.js
@@ -1,34 +1,55 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
 let randomNumber = Math.round(Math.random() * 10);
 
-function getGrade(total_mark){
-    let grade = "Ungraded"
+// function getGrade(total_mark){
+//     let grade = "Ungraded"
 
-    if (total_mark > 90){
-        grade = "A+";
-    }
 
-    else if (total_mark > 80 && total_mark <= 90 ){
-            grade = "A";
+    // switch(total_mark){
+    //     case total_mark > 90:
+    //         grade = "A+";
+    //         break;
 
-        }
+    //     case total_mark > 80 && total_mark <= 90:
+    //         grade = "A";
+    //         break;
+
+    //     case total_mark > 70 && total_mark <= 80:
+    //         grade = "B";
+    //         break;
+        
+    //     default 
+//     }
+//     if (total_mark > 90){
+//         grade = "A+";
+//     }
+
+//     else if (total_mark > 80 && total_mark <= 90 ){
+//             grade = "A";
+
+//         }
 
 
 
     
-console.log("You scored " + total_mark + " therefore your grade is " + grade)
+// console.log("You scored " + total_mark + " therefore your grade is " + grade)
     
-};
+// };
 
 
-getGrade(91)
+// getGrade(91)
 // ******************************** WHILE LOOPS **************************************
 
 
 
 
+x = 0
 
 
+while (x < 10){
+console.log(append.randomNumber);
+x += 1;
+}
 
 
 
