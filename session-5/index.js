8d6c3ee1032fc9fe273c49d37f625cb9be1f02f3
Victoria Adeniran <victoriaadeniran@Victorias-MacBook-Air.local>
2023-07-04 19:07:53 +0100

    Added if statements

--- session-5/index.js
+++ session-5/index.js
@@ -1,6 +1,24 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
 let randomNumber = Math.round(Math.random() * 10);
 
+function getGrade(total_mark){
+    let grade = "Ungraded"
+
+    if (total_mark > 90){
+        grade = "A+";
+    }
+
+    else if (total_mark > 80 && total_mark <= 90 ){
+            grade = "A";
+
+        }
+
+
+
+    console.log("You scored " + total_mark + " therefore your grade is " + grade)
+    getGrade(91)
+    
+}
 // ******************************** WHILE LOOPS **************************************
 
 
